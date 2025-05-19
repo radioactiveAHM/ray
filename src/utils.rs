b21d380e06c8c283e34346ef70467af16fc7b065
radioactiveAHM <djahmadfire@gmail.com>
2025-05-19 09:03:27 +0330

    make utils inline

--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,19 @@
+#[inline(always)]
 pub fn convert_two_u8s_to_u16_be(bytes: [u8; 2]) -> u16 {
     ((bytes[0] as u16) << 8) | bytes[1] as u16
 }
 
+#[inline(always)]
 pub fn convert_u16_to_two_u8s_be(integer: u16) -> [u8; 2] {
     [(integer >> 8) as u8, integer as u8]
 }
 
+#[inline(always)]
 pub fn unsafe_staticref<'a, T: ?Sized>(r: &'a T) -> &'static T {
     unsafe { std::mem::transmute::<&'a T, &'static T>(r) }
 }
 
+#[inline(always)]
 pub fn catch_in_buff(find: &[u8], buff: &[u8]) -> Option<(usize, usize)> {
     if find.len() >= buff.len() {
         return None;
