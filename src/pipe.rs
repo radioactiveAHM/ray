4143c6b8f7eb8f6f160a9337334b5edd25e2c933
radioactiveAHM <djahmadfire@gmail.com>
2025-11-13 17:20:47 +0330

    add tokio::task::coop for pipe

--- src/pipe.rs
+++ src/pipe.rs
@@ -39,14 +39,17 @@ where
         mut self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Self::Output> {
+        let coop = std::task::ready!(tokio::task::coop::poll_proceed(cx));
         let this = &mut *self;
-        std::task::ready!(this.0.as_mut().poll_read(cx, this.1)).map(|_| {
+        let poll = std::task::ready!(this.0.as_mut().poll_read(cx, this.1)).map(|_| {
             if this.1.filled().is_empty() {
                 std::task::Poll::Ready(Err(tokio::io::Error::other("Pipe read EOF")))
             } else {
                 std::task::Poll::Ready(Ok(()))
             }
-        })?
+        });
+        coop.made_progress();
+        poll?
     }
 }
 
@@ -61,6 +64,7 @@ where
         mut self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Self::Output> {
+        let coop = std::task::ready!(tokio::task::coop::poll_proceed(cx));
         let this = &mut *self;
         let mut filled = 0;
         loop {
@@ -69,10 +73,12 @@ where
                     if filled == 0 {
                         return std::task::Poll::Pending;
                     } else {
+                        coop.made_progress();
                         return std::task::Poll::Ready(Ok(()));
                     }
                 }
                 std::task::Poll::Ready(Ok(_)) => {
+                    coop.made_progress();
                     let fill = this.1.filled().len();
                     if fill == 0 || filled == fill {
                         return std::task::Poll::Ready(Err(tokio::io::Error::other(
@@ -83,7 +89,10 @@ where
                     }
                     filled = fill;
                 }
-                std::task::Poll::Ready(Err(e)) => return std::task::Poll::Ready(Err(e)),
+                std::task::Poll::Ready(Err(e)) => {
+                    coop.made_progress();
+                    return std::task::Poll::Ready(Err(e));
+                }
             };
         }
     }
